f90cc459cfc1181aec55c83a60c0830a647bf552
Kyle Harvey <kyleharv@gmail.com>
2020-04-02 22:56:57 -0400

    Initialize repository

---
+++ lib/kyleharv_palindrome.rb
@@ -0,0 +1,6 @@
+require "kyleharv_palindrome/version"
+
+module KyleharvPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kyleharv_palindrome/version.rb
@@ -0,0 +1,3 @@
+module KyleharvPalindrome
+  VERSION = "0.1.0"
+end
