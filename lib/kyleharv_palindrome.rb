0f2b4501d43cbfb8f1d519d4f65e24080b177266
Kyle Harvey <kyleharv@gmail.com>
2020-04-05 02:58:04 -0400

    Add integer testing to palindrome test suite and factor palindrome checker into module

--- lib/kyleharv_palindrome.rb
+++ lib/kyleharv_palindrome.rb
@@ -1,16 +1,28 @@
 require "kyleharv_palindrome/version"
 
-class String
+module KyleharvPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include KyleharvPalindrome
+end
+
+class Integer
+  include KyleharvPalindrome
 end
